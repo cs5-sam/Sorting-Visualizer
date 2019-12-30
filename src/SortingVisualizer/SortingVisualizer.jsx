1b335a9fef59ec87068300c3f2ab471c516e48fd
cs5-sam <54584505+cs5-sam@users.noreply.github.com>
2019-12-30 18:29:08 +0530

    Add files via upload
    Sorting Visualizer project.
    The SortingVisualizer component is imported and rendered in App.js.
    Everything below this line was automatically generated by Create React App.
    
    This project was bootstrapped with Create React App.
    
    npm run :
    
    Runs the app in the development mode.
    Open http://localhost:3000 to view it in the browser.
    
    The page will reload if you make edits.

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { getMergeSortAnimations } from '../sortingAlgorithms/sortingAlgorithms.js';
+import './SortingVisualizer.css';
+
+// Change this value for the speed of the animations.
+const speed = 1;
+
+// Change this value for the number of bars (value) in the array.
+const no_bars = 260;
+
+// This is the main color of the array bars.
+const bar_color = 'turquoise';
+
+// This is the color of array bars that are being compared throughout the animations.
+const compare_color = 'red';
+
+export default class SortingVisualizer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      array: [],
+    };
+  }
+
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  resetArray() {
+    const array = [];
+    for (let i = 0; i < no_bars; i++) {
+      array.push(randomIntFromInterval(5, 730));
+    }
+    this.setState({ array });
+  }
+
+  mergeSort() {
+    const animations = getMergeSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? compare_color : bar_color;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * speed);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * speed);
+      }
+    }
+  }
+
+  render() {
+    const { array } = this.state;
+
+    return (
+      <div className="array-container">
+        {array.map((value, idx) => (
+          <div
+            className="array-bar"
+            key={idx}
+            style={{
+              backgroundColor: bar_color,
+              height: `${value}px`,
+            }}></div>
+        ))}
+        <button onClick={() => this.resetArray()}>Generate New Array</button>
+        <button onClick={() => this.mergeSort()}>Merge Sort</button>
+
+
+
+      </div>
+    );
+  }
+}
+
+// From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function arraysAreEqual(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) return false;
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
